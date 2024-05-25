feb22af60077a2467c2fc49cfbe86f88abbdd845
bridgette-prpic <165822096+bridgette-prpic@users.noreply.github.com>
2024-05-25 12:35:56 +0200

    Injected more weather data

--- src/script.js
+++ src/script.js
@@ -2,10 +2,39 @@ function refreshWeatherData(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElemennt = document.querySelector("#humidity");
+  let windSpeedElemennt = document.querySelector("#wind-speed");
+  let timeElemennt = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
-
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElemennt.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElemennt.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElemennt.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
